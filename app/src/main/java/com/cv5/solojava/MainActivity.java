71c6bcb218cb41f159f2d4229088dc025517bd6f
Cristian Veras <mrchrist85@gmail.com>
2019-05-31 15:58:33 -0400

    Primer Commit

---
+++ app/src/main/java/com/cv5/solojava/MainActivity.java
@@ -0,0 +1,26 @@
+package com.cv5.solojava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView cantidad;
+    TextView precioTotal;
+    int intCantidad = 0;
+    int total;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        precioTotal = findViewById(R.id.precioTotal);
+        cantidad = findViewById(R.id.cantidad);
+    }
+    public void agregar(View view) {
+        intCantidad = intCantidad + 1;
+        total = intCantidad * 10;
+        cantidad.setText("" + intCantidad);
+        precioTotal.setText("RD$ "+total);
+    }
+}
